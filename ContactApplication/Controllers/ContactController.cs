101036de59c7afd1bb41bce537b30dfedb7b343d
tejeshliman <39603109+tejeshliman@users.noreply.github.com>
2018-05-25 02:52:09 +0530

    Commiting ContactApplicationWebAPI

---
+++ ContactApplication/Controllers/ContactController.cs
@@ -0,0 +1,51 @@
+﻿using DataAccessLayer;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SampleWebApi.Controllers
+{
+    public class ContactController : ApiController
+    {
+        IContactRepository contactRepo;
+
+        [Route("api/Contact/GetContacts")]
+        [HttpGet]
+        public IEnumerable<Contact> GetContacts()
+        {
+            contactRepo = new ContactRepository(ModelFactory<ContactDBContext>.GetContext());
+            return contactRepo.GetContacts();
+        }
+
+        [Route("api/Contact/GetContactById")]
+        [HttpGet]
+        public Contact GetContactById(int id)
+        {
+            contactRepo = new ContactRepository(ModelFactory<ContactDBContext>.GetContext());
+            return contactRepo.GetContactById(id);
+        }
+
+        [Route("api/Contact/AddContact")]
+        [HttpPost]
+        public Contact AddContact(Contact contact)
+        {
+            contactRepo = new ContactRepository(ModelFactory<ContactDBContext>.GetContext());
+            return contactRepo.AddContact(contact);
+        }
+
+        [Route("api/Contact/UpdateContact")]
+        [HttpPut]
+        public Contact UpdateContact([FromBody]Contact contact)
+        {
+            contactRepo = new ContactRepository(ModelFactory<ContactDBContext>.GetContext());
+            return contactRepo.UpdateContact(contact);
+        }
+
+        [Route("api/Contact/DeleteContact")]
+        [HttpDelete]
+        public bool DeleteContact([FromBody]int id)
+        {
+            contactRepo = new ContactRepository(ModelFactory<ContactDBContext>.GetContext());
+            return contactRepo.DeleteContact(id);
+        }
+    }
+}
